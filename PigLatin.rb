3581849c01a5fcf26fb5dc0bf256739a5b883c88
unknown <Owner@Owner-HP.(none)>
2013-06-04 16:06:59 -0400

    added sentence handling

--- PigLatin.rb
+++ PigLatin.rb
@@ -1,14 +1,28 @@
-def Pig_latin(string)
-	return nil if string.nil?
-	return "" if string.empty?
-	i=0
-	string.each_char do |c|  #returns position of first vowl
-		break if ["a","e","i","o","u"].include?(c)
-    i+=1
-  end
+class Pig_latin
 
-  string[i,string.size] + string[0, i] + "ay" #rearranges string at position of first vowl
+	def convert(word)
+		return nil if word.nil?
+		return "" if word.empty?
+		i=0
+		word.each_char do |c|  #returns position of first vowl
+			break if ["a","e","i","o","u"].include?(c)
+    	i+=1
+  	end
+
+  	word[i,word.size] + word[0, i] + "ay" #rearranges string at position of first vowl
+	end
+
+	def initialize(string)
+		return nil if string.nil?
+		return "" if string.empty?
+		converted_sent = ""
+
+		string.split.each do |word|
+			converted_sent << convert(word) + " "
+		end
+		puts converted_sent
+	end
 end
 
-puts "Enter a word to convert to pig latin"
-print Pig_latin(gets.chomp)
+puts "Enter a sentence to convert to pig latin"
+Pig_latin.new(gets.chomp)
